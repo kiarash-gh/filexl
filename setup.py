562b338555481b637bc27314ca8cdd9499511ed6
Kiarash-gh <kiarash.gh@gmail.com>
2025-04-29 08:25:25 +0330

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='filexl',
-    version='1.0.0',
+    version='1.0.1',
     description='CLI tool to list files and export to Excel',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
